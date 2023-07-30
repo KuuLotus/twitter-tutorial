abf756df6a3b2ab49d18e150cfc3618a5d30dd98
KuuLotus <kuulotusy@gmail.com>
2023-07-30 14:16:38 +0900

    [add] header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-  <div className='text-3xl text-sky-500'>
-    hello world
-  </div>
-  )
+    <>
+      <Header label="Home" />
+    </>
+  );
 }
