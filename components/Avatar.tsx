03a7a914f6fe336a929411208f0b71a437ccbc26
KuuLotus <kuulotusy@gmail.com>
2023-12-03 01:18:48 +0900

    [add] Avatat Component

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+import Image from "next/image";
+
+import useUser from "@/hooks/useUser";
 
 interface AvatarProps {
   userId: string;
@@ -6,12 +10,46 @@ interface AvatarProps {
   hasBorder?: boolean;
 }
 
-const Avatar: React.FC<AvatarProps> = ({
-  userId,
-  isLarge,
-  hasBorder
-}) => {
-  return <div></div>;
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  const { data: fetchedUser } = useUser(userId);
+  const router = useRouter();
+
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      const url = `/users/${userId}`;
+
+      router.push(url);
+    },
+    [router, userId]
+  );
+
+  return (
+    <div
+      className={`
+        ${hasBorder ? "border-4 border-black" : ""}
+        ${isLarge ? "h-32" : "h-12"}
+        ${isLarge ? "w-32" : "w-12"}
+        rounded-full
+        hover:opacity-90
+        transition
+        cursor-pointer
+        relative
+      `}
+    >
+      <Image
+        fill
+        style={{
+          objectFit: "cover",
+          borderRadius: "100%",
+        }}
+        alt="Avatar"
+        onClick={onClick}
+        src={fetchedUser?.profileImage || "/images/placeholder.png"}
+      />
+    </div>
+  );
 };
 
 export default Avatar;
