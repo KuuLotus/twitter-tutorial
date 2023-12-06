23b076f530a50ded93d3f4a9c65f08109b0c93e9
KuuLotus <kuulotusy@gmail.com>
2023-12-06 22:34:50 +0900

    [create] posts form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import { Inter } from "next/font/google";
+import Form from "@/components/Form";
 import Header from "@/components/Header";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What is happening?" />
     </>
   );
 }
